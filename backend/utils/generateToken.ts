f483dc7b47621a365a202b4d4d797c407646000c
Raman maharjan <ramanmaharzan123@gmail.com>
2025-10-27 13:03:50 +0545

    user model created

---
+++ backend/utils/generateToken.ts
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken";
+
+export const generateTokenAndSetCookie = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+  res.cookie("jwt", token, {
+    maxAge: 24 * 60 * 60,
+    httpOnly: true,
+    sameSite: "strict",
+  });
+};
