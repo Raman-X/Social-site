b334f813be1d17eb4e5843b2fba2473521b44a64
Raman maharjan <ramanmaharzan123@gmail.com>
2025-10-27 13:06:07 +0545

    sign up controller created

--- backend/routes/auth.routes.ts
+++ backend/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-// import { login, logout, signup } from "../controllers/auth.controller.ts";
+import { login, logout, signup } from "../controllers/auth.controller.ts";
 
 const router = express.Router();
 router.get("/signup", signup);
