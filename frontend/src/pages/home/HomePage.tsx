c6a9391a693c653abb474de0c803586c86dbf20f
Raman maharjan <ramanmaharzan123@gmail.com>
2025-10-29 11:50:50 +0545

    added home page ui and dummy data

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,5 +1,43 @@
-const HomePage = () => {
-  return <div>home</div>;
-};
+import { useState } from "react";
 
+import Posts from "../../components/common/Posts";
+import CreatePost from "./CreatePost";
+
+const HomePage = () => {
+  const [feedType, setFeedType] = useState("forYou");
+
+  return (
+    <div className={""}>
+      <div className="flex-[4_4_0] mr-auto border-x border-gray-700 min-h-screen">
+        {/* header */}
+        <div className="flex w-full ">
+          <div
+            className={
+              "flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
+            }
+            onClick={() => setFeedType("forYou")}
+          >
+            For you
+            {feedType === "forYou" && (
+              <div className="absolute bottom-0 w-10  h-1 rounded-full bg-primary"></div>
+            )}
+          </div>
+          <div
+            className="flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
+            onClick={() => setFeedType("following")}
+          >
+            Following
+            {feedType === "following" && (
+              <div className="absolute bottom-0 w-10  h-1 rounded-full bg-primary"></div>
+            )}
+          </div>
+        </div>
+
+        <CreatePost />
+
+        <Posts />
+      </div>
+    </div>
+  );
+};
 export default HomePage;
