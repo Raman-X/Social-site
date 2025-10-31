d591d5c1ad58f07d8b45a2dc31ed7165c5439e59
Raman maharjan <ramanmaharzan123@gmail.com>
2025-10-31 16:30:31 +0545

    create live stream controller created

--- backend/controllers/live-stream.controller.ts
+++ backend/controllers/live-stream.controller.ts
@@ -12,3 +12,24 @@ export const getAllLiveStreams = async (req: Request, res: Response) => {
     return res.status(500).json("internal server error");
   }
 };
+
+export const createLiveStream = async (req: Request, res: Response) => {
+  try {
+    const { name, url } = req.body;
+
+    if (!name || !url) {
+      return res.status(400).json({ error: "url or name not given" });
+    }
+
+    const liveStream = new LiveStream({
+      user: (req as any).user._id,
+      name: name,
+      url: url,
+    });
+    await liveStream.save();
+
+    res.status(201).json(liveStream);
+  } catch (error: unknown) {
+    return res.status(500).json("internal server error");
+  }
+};
--- backend/routes/live-stream.route.ts
+++ backend/routes/live-stream.route.ts
@@ -1,9 +1,13 @@
 import express from "express";
-import { getAllLiveStreams } from "../controllers/live-stream.controller";
+import {
+  createLiveStream,
+  getAllLiveStreams,
+} from "../controllers/live-stream.controller";
 import { protectRoute } from "../middleware/protectRoute";
 
 const router = express.Router();
 
 router.get("/all", protectRoute, getAllLiveStreams);
+router.post("/create", protectRoute, createLiveStream);
 
 export default router;
